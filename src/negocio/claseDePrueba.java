4685bbc5d576e447f7ac9f607242993ed949df81
Alejandro Pacheco1202 <alep1202@gmail.com>
2023-04-24 08:09:44 -0300

    agregue una clase de prueba

---
+++ src/negocio/claseDePrueba.java
@@ -0,0 +1,11 @@
+package negocio;
+
+public class claseDePrueba {
+
+	
+	/* 1- volvi a clonar pero en otra carpeta.
+	 * 2- agregue esta clase de prueba y ahora si me lo permitio
+	 * 
+	 * 
+	 */
+}
